be31fd22cdf6ecab53eb434d0c4288b01b065a04
ChurchillV <vincechurchillankrah@gmail.com>
2024-05-08 14:58:06 +0000

    Restructured interface, logic. Letter animations

--- script.js
+++ script.js
@@ -30,6 +30,8 @@ const answer = 'logic';
 // User input logic
 // Store all rows of user input
 const rows = document.querySelectorAll('.guess-row');
+const userTiles = document.querySelectorAll('.user-input-tile');
+
 // Submit button appears once a user fills a row
 let submitButton = document.getElementById('submit-button');
 
@@ -51,16 +53,12 @@ function focusFirstTileOfNextRow(currentRow) {
 }
 
 // Function to display alert box on winning
-function displayAlertBox() {
+function displaySuccess() {
     document.body.classList.add('bg-slate-400');
     alertBox.classList.remove('hidden');
     alertBox.classList.add('grid');
-    console.log('Checking');
 
-    // Select all elements with the class user-input-tile
-    const userTiles = document.querySelectorAll('.user-input-tile');
-
-    // Loop through each element and set contenteditable to false
+    // Loop through each input tile and set contenteditable to false
     userTiles.forEach(tile => {
         tile.contentEditable = 'false';
     });  
@@ -79,14 +77,71 @@ resetButton.addEventListener('click', function() {
     location.reload();
 })
 
+/**
+ * Iterate through each tile and check whether it matches the letter at
+ * the corresponding index.
+// 🟩 - In the word, correct position
+// 🟨 - In the word, wrong position
+// 🔴 - Not in the word
+ * 
+ */
+async function analyseAnswer() {
+    // Create a promise to handle the asynchronous setTimeout calls
+    return new Promise(resolve => {
+        userTiles.forEach((tile, index) => {
+            const letter = answer[index];
+            const userInput = tile.innerText.trim().toLowerCase();
+            console.log('Checking');
 
-function validateAnswer(input) {
+            setTimeout(() => {
+                tile.classList.add('text-white');
+                if (userInput === letter) {
+                    tile.classList.add('bg-green-500'); // 🟩 
+                } else if (answer.includes(userInput)) {
+                    console.log('Yellow');
+                    tile.classList.add('bg-yellow-500'); // 🟨 
+                } else {
+                    console.log('Red');
+                    tile.classList.add('bg-red-500'); // 🔴
+                }
+
+                // Resolve the promise after all setTimeout calls are completed
+                if (index+1 === userTiles.length) {
+                    setTimeout(()=> {
+                        resolve();
+                    }, 1000);
+                }
+            }, index * 500);
+        });
+    });
+}
+
+/**
+ * Reset all tiles to original color and clear text
+ */
+function resetTiles() {
+    userTiles.forEach((tile, index) => {
+        setTimeout(() => {
+            tile.classList.remove('bg-green-500', 'bg-yellow-500', 'bg-red-500', 'text-white');
+            tile.innerText = '';
+        }, index*500);
+    })
+
+    userTiles[0].focus();
+}
+
+
+async function validateAnswer(input) {
+    await analyseAnswer();
+
     if(/^[a-zA-Z]{5}/.test(input)) {
-        input.toLowerCase() == answer ? displayAlertBox()
+        input.toLowerCase() == answer ? displaySuccess()
                                       : alert('Not quite the answer')
     } else {
         alert("That's not a word");
     }
+
+    resetTiles();
 }
 
 let word = '';
